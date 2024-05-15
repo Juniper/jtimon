9e89616f71a3df356aff18b085b766af9112b80e
M-Vivek-Juniper <vm@juniper.net>
2024-05-15 17:32:32 +0530

    UT for issues found during CCL

--- subscribe_gnmi_test.go
+++ subscribe_gnmi_test.go
@@ -617,6 +617,237 @@ func TestGnmiHandleResponse(t *testing.T) {
 	*noppgoroutines = false
 }
 
+func TestMeasurementNameInGnmiParseHeader(t *testing.T) {
+	*noppgoroutines = true
+	*prom = true
+	gGnmiUnitTestCoverage = true
+
+	var hdrInputExtTerseHeaderForIPAFT = gnmi_juniper_header_ext.GnmiJuniperTelemetryHeaderExtension{
+		SequenceNumber: 1,
+	}
+
+	hdrInputExtTerseHeaderForIPAFTBytes, err := proto.Marshal(&hdrInputExtTerseHeaderForIPAFT)
+	if err != nil {
+		t.Errorf("Error marshalling header for ext isync case: %v", err)
+	}
+
+	var hdrInputExtHeaderWithSubscribedPath = gnmi_juniper_header_ext.GnmiJuniperTelemetryHeaderExtension{
+		SequenceNumber: 1, SubscribedPath: "/x/y/z/",
+	}
+
+	hdrInputExtHeaderWithSubscribedPathBytes, err := proto.Marshal(&hdrInputExtHeaderWithSubscribedPath)
+	if err != nil {
+		t.Errorf("Error marshalling header for ext isync case: %v", err)
+	}
+
+	tests := []struct {
+		name                    string
+		jctx                    *JCtx
+		rsp                     *gnmi.SubscribeResponse
+		parseOutput             *gnmiParseOutputT
+		expectedMeasurementName string
+		err                     bool
+	}{
+		{
+			name:                    "rsp-check-juniper-terse-header-ipaft-with-prefix",
+			expectedMeasurementName: "/network-instances/network-instance/afts/next-hop-groups/next-hop-group/",
+			err:                     false,
+			jctx: &JCtx{
+				config: Config{
+					Host: "127.0.0.1",
+					Port: 32767,
+					Log: LogConfig{
+						Verbose: true,
+					},
+				},
+			},
+			parseOutput: &gnmiParseOutputT{
+				kvpairs: map[string]string{},
+				xpaths:  map[string]interface{}{},
+			},
+			rsp: &gnmi.SubscribeResponse{
+				Extension: []*gnmi_ext1.Extension{
+					{
+						Ext: &gnmi_ext1.Extension_RegisteredExt{
+							RegisteredExt: &gnmi_ext1.RegisteredExtension{
+								Id:  gnmi_ext1.ExtensionID_EID_JUNIPER_TELEMETRY_HEADER,
+								Msg: hdrInputExtTerseHeaderForIPAFTBytes,
+							},
+						},
+					},
+				},
+				Response: &gnmi.SubscribeResponse_Update{
+					Update: &gnmi.Notification{
+						Timestamp: 1589476296083000000,
+						Prefix: &gnmi.Path{
+							Origin: "",
+							Elem: []*gnmi.PathElem{
+								{Name: "network-instances"},
+								{Name: "network-instance", Key: map[string]string{"name": "DEFAULT"}},
+								{Name: "afts"},
+								{Name: "next-hop-groups"},
+								{Name: "next-hop-group", Key: map[string]string{"id": "1"}},
+							},
+						},
+						Update: []*gnmi.Update{
+							{
+								Path: &gnmi.Path{
+									Origin: "",
+									Elem: []*gnmi.PathElem{
+										{Name: "state"},
+										{Name: "dummyfield"},
+									},
+								},
+								Val: &gnmi.TypedValue{
+									Value: &gnmi.TypedValue_StringVal{StringVal: "dummyval"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:                    "rsp-check-juniper-terse-header-without-prefix",
+			expectedMeasurementName: "/",
+			err:                     false,
+			jctx: &JCtx{
+				config: Config{
+					Host: "127.0.0.1",
+					Port: 32767,
+					Log: LogConfig{
+						Verbose: true,
+					},
+				},
+			},
+			parseOutput: &gnmiParseOutputT{
+				kvpairs: map[string]string{},
+				xpaths:  map[string]interface{}{},
+			},
+			rsp: &gnmi.SubscribeResponse{
+				Extension: []*gnmi_ext1.Extension{
+					{
+						Ext: &gnmi_ext1.Extension_RegisteredExt{
+							RegisteredExt: &gnmi_ext1.RegisteredExtension{
+								Id:  gnmi_ext1.ExtensionID_EID_JUNIPER_TELEMETRY_HEADER,
+								Msg: hdrInputExtTerseHeaderForIPAFTBytes,
+							},
+						},
+					},
+				},
+				Response: &gnmi.SubscribeResponse_Update{
+					Update: &gnmi.Notification{
+						Timestamp: 1589476296083000000,
+						Update: []*gnmi.Update{
+							{
+								Path: &gnmi.Path{
+									Origin: "",
+									Elem: []*gnmi.PathElem{
+										{Name: "state"},
+										{Name: "dummyfield"},
+									},
+								},
+								Val: &gnmi.TypedValue{
+									Value: &gnmi.TypedValue_StringVal{StringVal: "dummyval"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:                    "rsp-check-juniper-header-with-subscribed-path",
+			expectedMeasurementName: hdrInputExtHeaderWithSubscribedPath.SubscribedPath,
+			err:                     false,
+			jctx: &JCtx{
+				config: Config{
+					Host: "127.0.0.1",
+					Port: 32767,
+					Log: LogConfig{
+						Verbose: true,
+					},
+				},
+			},
+			parseOutput: &gnmiParseOutputT{
+				kvpairs: map[string]string{},
+				xpaths:  map[string]interface{}{},
+			},
+			rsp: &gnmi.SubscribeResponse{
+				Extension: []*gnmi_ext1.Extension{
+					{
+						Ext: &gnmi_ext1.Extension_RegisteredExt{
+							RegisteredExt: &gnmi_ext1.RegisteredExtension{
+								Id:  gnmi_ext1.ExtensionID_EID_JUNIPER_TELEMETRY_HEADER,
+								Msg: hdrInputExtHeaderWithSubscribedPathBytes,
+							},
+						},
+					},
+				},
+				Response: &gnmi.SubscribeResponse_Update{
+					Update: &gnmi.Notification{
+						Timestamp: 1589476296083000000,
+						Update: []*gnmi.Update{
+							{
+								Path: &gnmi.Path{
+									Origin: "",
+									Elem: []*gnmi.PathElem{
+										{Name: "state"},
+										{Name: "dummyfield"},
+									},
+								},
+								Val: &gnmi.TypedValue{
+									Value: &gnmi.TypedValue_StringVal{StringVal: "dummyval"},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			parseOutput, err := gnmiParseNotification(false, test.rsp, test.parseOutput, true)
+			if err != nil && strings.Contains(err.Error(), gGnmiJtimonIgnoreErrorSubstr) {
+				err = nil
+			}
+			if err != nil {
+				errMsg := fmt.Sprintf("didn't expect error:%v", err)
+				t.Errorf(errMsg)
+			}
+
+			parseOutput, err = gnmiParseHeader(test.rsp, parseOutput)
+			if !test.err {
+				if err != nil && strings.Contains(err.Error(), gGnmiJtimonIgnoreErrorSubstr) {
+					err = nil
+				}
+				if err != nil {
+					errMsg := fmt.Sprintf("didn't expect error:%v", err)
+					t.Errorf(errMsg)
+				}
+				if parseOutput.mName != test.expectedMeasurementName {
+					errMsg := fmt.Sprintf("Measurement name, expected: %v, got: %v",
+						test.expectedMeasurementName, parseOutput.mName)
+					t.Errorf(errMsg)
+				}
+			}
+
+			if test.err {
+				if err == nil {
+					errMsg := "expected error"
+					t.Errorf(errMsg)
+				}
+			}
+		})
+	}
+
+	*noppgoroutines = false
+	*prom = false
+	gGnmiUnitTestCoverage = false
+}
+
 // For the below function, better change jtisim?
 func TestSubscribegNMI(t *testing.T) {
 }
