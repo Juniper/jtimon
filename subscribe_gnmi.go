648d78f1de31bd796780319c9763b0f22a5f151c
amohit <amohit@juniper.net>
2026-01-14 11:51:23 +0530

    Added formatting and once subscription support

--- subscribe_gnmi.go
+++ subscribe_gnmi.go
@@ -503,7 +503,6 @@ func subscribegNMI(conn *grpc.ClientConn, jctx *JCtx, cfg Config, paths []PathsC
 
 	// 1. Form request
 	subs.Mode = gnmi.SubscriptionList_Mode(cfg.Vendor.Gnmi.Mode)
-	jLog(jctx, fmt.Sprintf("test: gNMI host: %v, Subscription mode: %v", hostname, subs.Mode.String()))
 	// PROTO encoding
 	if jctx.config.Vendor.Gnmi != nil {
 		switch jctx.config.Vendor.Gnmi.Encoding {
