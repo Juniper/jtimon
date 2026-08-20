fbffeb807def15a0adbc38dbd07f07a2d26023df
Sarath Gollapudi <sarathg@juniper.net>
2026-08-20 17:57:59 +0000

    Window Size is not set by default

--- grpc.go
+++ grpc.go
@@ -65,8 +65,10 @@ func getGPRCDialOptions(jctx *JCtx, vendor *vendor) ([]grpc.DialOption, error) {
 	}
 
 	ws := jctx.config.GRPC.WS
-	opts = append(opts, grpc.WithInitialWindowSize(ws))
-	opts = append(opts, grpc.WithInitialConnWindowSize(ws))
+	if ws != 0 {
+		opts = append(opts, grpc.WithInitialWindowSize(ws))
+		opts = append(opts, grpc.WithInitialConnWindowSize(ws))
+	}
 
 	if vendor.dialExt != nil {
 		opt := vendor.dialExt(jctx)
